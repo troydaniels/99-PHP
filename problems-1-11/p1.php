c92a5d887a0d8fdf0bc8b1a8593aca3610ea6c28
Troy Daniels <troy_daniels@live.com.au>
2015-06-14 16:02:25 +1000

    Problem 1

---
+++ problems-1-11/p1.php
@@ -0,0 +1,23 @@
+<?php
+//Function that returns the last element of a list
+function last(array $a){
+   $items=count($a);
+   $last=NULL;
+   $i=0;
+   foreach($a as $key=>$value){
+      if(++$i==$items){
+         $last=$value;
+      }
+   }
+   return $last;
+}
+
+//Tests
+$b=array(1,2,3,4,5);
+assert(last($b)==5);
+$c=array("Mary", "Kate", "Ashley");
+assert(last($c)=="Ashley");
+$d=array('fruit'=>'apple','veggie'=>'carrot');
+assert(last($d)=="carrot");
+
+?>
