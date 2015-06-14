7362a10c69f2b77b29f7064318ac78291547ec62
Troy Daniels <troy_daniels@live.com.au>
2015-06-14 17:43:47 +1000

    Problem 5

---
+++ problems-1-11/p5.php
@@ -0,0 +1,23 @@
+<?php
+//Function that returns a reversed array
+//As all arrays in PHP are hashes, this function will reverse the order of the
+//values, and assign them back to the keys
+function reverse(array $a){
+   $keys=array_values($a);
+   $items=count($a);
+   $reversedArray=array();
+   $i=1;
+   foreach($a as $key=>$value){
+      $reversedArray[$key]=$keys[$items-$i];
+      $i++;
+   }
+   return $reversedArray;
+}
+
+//Tests
+$b=array(1,2,3,4,5);
+assert(reverse($b)==array(5,4,3,2,1));
+$c=array('fruit'=>'apple','veggie'=>'carrot');
+assert(reverse($c)==array('fruit'=>'carrot','veggie'=>'apple'));
+
+?>
