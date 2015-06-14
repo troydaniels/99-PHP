14c97d7ad4b990689884f6f2e0e5697256ee344d
Troy Daniels <troy_daniels@live.com.au>
2015-06-14 21:40:39 +1000

    Problem 7

---
+++ problems-1-11/p7.php
@@ -0,0 +1,19 @@
+<?php
+//Flatten a nested array structure
+function flatten($a){
+   $flatArray=array();
+   if(is_array($a)){
+      foreach($a as $element){
+         $flatArray=array_merge($flatArray, flatten($element));
+      }
+   }else{
+      $flatArray[]=$a;
+   }
+   return $flatArray;
+}
+
+//Tests
+$b=array(1,2,array(3,array(4)),array(5,6),7);
+assert(flatten($b)==array(1,2,3,4,5,6,7));
+
+?>
