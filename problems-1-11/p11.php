2ccd1e4e0c0af642ef888f41146d84b129a9c997
Troy Daniels <troy_daniels@live.com.au>
2015-06-15 01:30:56 +1000

    Problem 11

---
+++ problems-1-11/p11.php
@@ -0,0 +1,33 @@
+<?php
+//Modified run length encoding of an array
+//This implementation does not maintain keys from original array
+function modifiedEncode(array $a){
+   $encodedArray=array();
+   $items=count($a);
+   $keys=array_keys($a);
+   for($i=0; $i<$items; $i++){
+      $k=1;                                 //Run counter
+      $encodedArray[]=$a[$keys[$i]];        //run==1, add single value to array
+      $subArray=array();
+     for($j=$i+1; $j<$items; $j++){
+         if($a[$keys[$i]]==$a[$keys[$j]]){
+            array_pop($encodedArray);       //run>1, remove last element
+            unset($subArray[$k]);
+            $subArray[++$k]=$a[$keys[$i]];  //Increment run counter
+            $i++;
+            $encodedArray[]=$subArray;      //And add run array to encoding
+         }
+      }
+      unset($subArray);
+   }
+   return $encodedArray;
+}
+
+//Tests
+$b=array(1,2,2,3,4,5,5,5,6,7);
+$c=array(1, array('2'=>'2'), 3, 4, array('3'=>'5'), 6, 7);
+assert(modifiedEncode($b)==$c);
+$d=array('apple'=>'juice', 'banana'=>'juice', 'prune'=>'juice', 'carrot'=>'cake', 'steak'=>'pie', 'chicken'=>'pie');
+$e=array(array('3'=>'juice'), 'cake', array('2'=>'pie'));
+assert(modifiedEncode($d)==$e);
+?>
