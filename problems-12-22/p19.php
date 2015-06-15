a9aa2ea60fccb315dec8686b4b42561ec58aa8d0
Troy Daniels <troy_daniels@live.com.au>
2015-06-16 00:09:47 +1000

    Problem 20

--- problems-12-22/p19.php
+++ problems-12-22/p19.php
@@ -1,12 +1,14 @@
 <?php
 //Rotate an array N places to the left
-//Keys are not maintained in this implementation
+//Keys positions are maintained in this implementation
 function rotate(array $a, $places){
    $partition=splitArray($a, $places);
    $rotatedArray=array();
+   $position=0;
+   $keys=array_keys($a);
    foreach(array_reverse($partition) as $section){
       foreach($section as $key=>$value){
-         $rotatedArray[]=$value;
+         $rotatedArray[$keys[$position++]]=$value;
       }
    }
    return $rotatedArray;
@@ -32,5 +34,7 @@ function splitArray(array $a, $splitNumber){
 $b=array(1,2,3,4,5,6,7);
 $c=array(3,4,5,6,7,1,2);
 assert(rotate($b,2)==$c);
-
+$d=array('foo'=>'bar', 'baz'=>'far', 'boo'=>'faz');
+$e=array('foo'=>'faz', 'baz'=>'bar', 'boo'=>'far');
+assert(rotate($d,2)==$e);
 ?>
---
+++ problems-12-22/p20.php
@@ -0,0 +1,22 @@
+<?php
+//Remove the K'th element from a list
+function removeAt(array $a, $element){
+   $position=1;
+   $result=array();
+   foreach($a as $key=>$value){
+      if($element!=$position++){
+         $result[$key]=$value;
+      }
+   }
+   return $result;
+}
+
+//Tests
+$b=array(1,2,3,4,5,6,7);
+$c=array(1,2,3,4=>5,6,7);
+assert(removeAt($b,4)==$c);
+$d=array('foo'=>'bar', 'baz'=>'far', 'boo'=>'faz');
+$e=array('baz'=>'far', 'boo'=>'faz');
+assert(removeAt($d,1)==$e);
+
+?>
