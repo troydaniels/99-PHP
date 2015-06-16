338099e9da43e6e9ec59f07592d35f7442992736
Web Profits <webprofits@Troy-Daniels.local>
2015-06-16 15:08:48 +1000

    Problem 22

---
+++ problems-12-23/p22.php
@@ -0,0 +1,19 @@
+<?php
+//Create an array containing all integers within a given range.
+//If first argument is smaller than second, produce an array in decreasing order.
+function arrayRange($a, $b){
+   $step=($a<$b)?1:-1;
+   $result=array();
+   for(; $a!=$b+$step; $a+=$step)
+      $result[]=$a;
+   return $result;
+}
+
+//Tests
+$c=array(-1,0,1,2,3);
+assert(arrayRange(-1,3)==$c);
+$d=array(1);
+assert(arrayRange(1,1)==$d);
+$e=array(3,2,1);
+assert(arrayRange(3,1)==$e);
+?>
